5a7b98caca0910ae09050cbc400c4c64390a36a7
Bryan <secretgenius@hotmail.com>
2012-08-29 19:59:11 -0700

    Added a value editor to the project.

--- HubrisEditor/Core/GameComponentRoot.cs
+++ HubrisEditor/Core/GameComponentRoot.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace HubrisEditor.Core
 {
-    public class GameComponentRoot : INotifyPropertyChanged
+    public class GameComponentRoot : INotifyPropertyChanged, INotifyPropertyChanging
     {
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
@@ -17,6 +17,18 @@ namespace HubrisEditor.Core
             if (PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        #region INotifyPropertyChanging
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        public void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
             }
         }
         #endregion
---
+++ HubrisEditor/Xaml/Controls/ValueEditor.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HubrisEditor.Xaml.Controls
+{
+    [TemplatePart(Name = "PART_EditorField", Type = typeof(TextBox))]
+    [TemplatePart(Name = "PART_DisplayField", Type = typeof(TextBlock))]
+    public class ValueEditor : Control
+    {
+        static ValueEditor()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ValueEditor), new FrameworkPropertyMetadata(typeof(ValueEditor)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            m_editorField = GetTemplateChild("PART_EditorField") as TextBox;
+            m_displayField = GetTemplateChild("PART_DisplayField") as TextBlock;
+
+            m_editorField.Visibility = System.Windows.Visibility.Collapsed;
+            m_displayField.Visibility = System.Windows.Visibility.Visible;
+
+            Binding editorBinding = new Binding("Text");
+            editorBinding.Source = this;
+            editorBinding.Mode = BindingMode.TwoWay;
+            editorBinding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            BindingOperations.SetBinding(m_editorField, TextBox.TextProperty, editorBinding);
+
+            Binding displayBinding = new Binding("Text");
+            displayBinding.Source = this;
+            displayBinding.Mode = BindingMode.OneWay;
+            displayBinding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            BindingOperations.SetBinding(m_displayField, TextBlock.TextProperty, displayBinding);
+
+            PreviewMouseDown += ValueEditor_PreviewMouseDown;
+            PreviewKeyDown += ValueEditor_PreviewKeyDown;
+        }
+
+        private void ValueEditor_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (m_editMode)
+            {
+                if (e.Key == Key.Enter)
+                {
+                    m_editMode = false;
+                    e.Handled = true;
+                    m_editorField.Visibility = System.Windows.Visibility.Collapsed;
+                    m_displayField.Visibility = System.Windows.Visibility.Visible;
+                }
+            }
+        }
+
+        private void ValueEditor_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!m_editMode)
+            {
+                m_editMode = true;
+                e.Handled = true;
+                m_editorField.Visibility = System.Windows.Visibility.Visible;
+                m_displayField.Visibility = System.Windows.Visibility.Collapsed;
+                m_editorField.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        m_editorField.RaiseEvent(new MouseButtonEventArgs(e.MouseDevice, e.Timestamp, e.ChangedButton, e.StylusDevice) { RoutedEvent = TextBox.MouseDownEvent });
+                    }), System.Windows.Threading.DispatcherPriority.Background, null);
+            }
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(ValueEditor), new PropertyMetadata(string.Empty));
+
+        private TextBox m_editorField;
+        private TextBlock m_displayField;
+        private bool m_editMode;
+    }
+}
