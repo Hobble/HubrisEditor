32e12b2432e39fc713864070d4cbba3f3627bd62
Bryan <secretgenius@hotmail.com>
2012-10-07 22:55:03 -0700

    Refactored some code.

--- HubrisEditor/Core/PerlinNoise.cs
+++ HubrisEditor/Core/PerlinNoise.cs
@@ -8,33 +8,9 @@ namespace HubrisEditor.Core
 {
     public class PerlinNoise
     {
-        private const int GradientSizeTable = 256;
-        private readonly Random _random;
-        private readonly float[] _gradients = new float[GradientSizeTable * 3];
-
-        private readonly byte[] _perm = new byte[]
-        {
-            225,155,210,108,175,199,221,144,203,116, 70,213, 69,158, 33,252,
-            5, 82,173,133,222,139,174, 27,  9, 71, 90,246, 75,130, 91,191,
-            169,138,  2,151,194,235, 81,  7, 25,113,228,159,205,253,134,142,
-            248, 65,224,217, 22,121,229, 63, 89,103, 96,104,156, 17,201,129,
-            36,  8,165,110,237,117,231, 56,132,211,152, 20,181,111,239,218,
-            170,163, 51,172,157, 47, 80,212,176,250, 87, 49, 99,242,136,189,
-            162,115, 44, 43,124, 94,150, 16,141,247, 32, 10,198,223,255, 72,
-            53,131, 84, 57,220,197, 58, 50,208, 11,241, 28,  3,192, 62,202,
-            18,215,153, 24, 76, 41, 15,179, 39, 46, 55,  6,128,167, 23,188,
-            106, 34,187,140,164, 73,112,182,244,195,227, 13, 35, 77,196,185,
-            26,200,226,119, 31,123,168,125,249, 68,183,230,177,135,160,180,
-            12,  1,243,148,102,166, 38,238,251, 37,240,126, 64, 74,161, 40,
-            184,149,171,178,101, 66, 29, 59,146, 61,254,107, 42, 86,154,  4,
-            236,232,120, 21,233,209, 45, 98,193,114, 78, 19,206, 14,118,127,
-            48, 79,147, 85, 30,207,219, 54, 88,234,190,122, 95, 67,143,109,
-            137,214,145, 93, 92,100,245,  0,216,186, 60, 83,105, 97,204, 52
-        };
-
         public PerlinNoise(int seed)
         {
-            _random = new Random(seed);
+            m_random = new Random(seed);
             InitGradients();
         }
 
@@ -81,19 +57,19 @@ namespace HubrisEditor.Core
         {
             for (int i = 0; i < GradientSizeTable; i++)
             {
-                float z = 1f - 2f * (float)_random.NextDouble();
+                float z = 1f - 2f * (float)m_random.NextDouble();
                 float r = (float)Math.Sqrt(1f - z * z);
-                float theta = 2 * (float)Math.PI * (float)_random.NextDouble();
-                _gradients[i * 3] = r * (float)Math.Cos(theta);
-                _gradients[i * 3 + 1] = r * (float)Math.Sin(theta);
-                _gradients[i * 3 + 2] = z;
+                float theta = 2 * (float)Math.PI * (float)m_random.NextDouble();
+                m_gradients[i * 3] = r * (float)Math.Cos(theta);
+                m_gradients[i * 3 + 1] = r * (float)Math.Sin(theta);
+                m_gradients[i * 3 + 2] = z;
             }
         }
 
         private int Permutate(int x)
         {
             const int mask = GradientSizeTable - 1;
-            return _perm[x & mask];
+            return m_perm[x & mask];
         }
 
         private int Index(int ix, int iy, int iz)
@@ -105,7 +81,7 @@ namespace HubrisEditor.Core
         {
             int index = Index(ix, iy, iz);
             int g = index * 3;
-            return _gradients[g] * fx + _gradients[g + 1] * fy + _gradients[g + 2] * fz;
+            return m_gradients[g] * fx + m_gradients[g + 1] * fy + m_gradients[g + 2] * fz;
         }
 
         private float Lerp(float t, float value0, float value1)
@@ -117,5 +93,29 @@ namespace HubrisEditor.Core
         {
             return x * x * (3 - 2 * x);
         }
+
+        private const int GradientSizeTable = 256;
+        private readonly Random m_random;
+        private readonly float[] m_gradients = new float[GradientSizeTable * 3];
+
+        private readonly byte[] m_perm = new byte[]
+        {
+              225, 155, 210, 108, 175, 199, 221, 144, 203, 116,  70, 213,  69, 158,  33, 252,
+                5,  82, 173, 133, 222, 139, 174,  27,   9,  71,  90, 246,  75, 130,  91, 191,
+              169, 138,   2, 151, 194, 235,  81,   7,  25, 113, 228, 159, 205, 253, 134, 142,
+              248,  65, 224, 217,  22, 121, 229,  63,  89, 103,  96, 104, 156,  17, 201, 129,
+               36,   8, 165, 110, 237, 117, 231,  56, 132, 211, 152,  20, 181, 111, 239, 218,
+              170, 163,  51, 172, 157,  47,  80, 212, 176, 250,  87,  49,  99, 242, 136, 189,
+              162, 115,  44,  43, 124,  94, 150,  16, 141, 247,  32,  10, 198, 223, 255,  72,
+               53, 131,  84,  57, 220, 197,  58,  50, 208,  11, 241,  28,   3, 192,  62, 202,
+               18, 215, 153,  24,  76,  41,  15, 179,  39,  46,  55,   6, 128, 167,  23, 188,
+              106,  34, 187, 140, 164,  73, 112, 182, 244, 195, 227,  13,  35,  77, 196, 185,
+               26, 200, 226, 119,  31, 123, 168, 125, 249,  68, 183, 230, 177, 135, 160, 180,
+               12,   1, 243, 148, 102, 166,  38, 238, 251,  37, 240, 126,  64,  74, 161,  40,
+              184, 149, 171, 178, 101,  66,  29,  59, 146,  61, 254, 107,  42,  86, 154,   4,
+              236, 232, 120,  21, 233, 209,  45,  98, 193, 114,  78,  19, 206,  14, 118, 127,
+               48,  79, 147,  85,  30, 207, 219,  54,  88, 234, 190, 122,  95,  67, 143, 109,
+              137, 214, 145,  93,  92, 100, 245,   0, 216, 186,  60,  83, 105,  97, 204,  52
+        };
     }
 }
